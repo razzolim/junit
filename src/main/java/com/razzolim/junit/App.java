23d7cc91db2a74e6e9ce1e595d70668ae1ff0899
Renan Azzolim <azzolimrenan2@gmail.com>
2021-01-28 20:17:56 -0300

    preparação e inclusão junit 5

--- src/main/java/com/razzolim/junit/App.java
+++ src/main/java/com/razzolim/junit/App.java
@@ -4,10 +4,17 @@ package com.razzolim.junit;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+	private static final String HELLO = "Hello";
+	private static final String WORLD = "World";
+
+	public String helloWorld() {
+		return HELLO + " " + WORLD;
+	}
+
+	public String helloWorld(String name) {
+		return HELLO + " " + name;
+	}
+
 }
