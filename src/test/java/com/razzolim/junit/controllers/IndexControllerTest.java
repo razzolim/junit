9a429423cf6ed6fa8c1b9a40ad054effd74b4048
Renan Azzolim <azzolimrenan2@gmail.com>
2021-02-01 19:34:28 -0300

    Conditional JUnit Test Execution

--- src/test/java/com/razzolim/junit/controllers/IndexControllerTest.java
+++ src/test/java/com/razzolim/junit/controllers/IndexControllerTest.java
@@ -12,6 +12,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.junit.jupiter.api.condition.EnabledOnJre;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.JRE;
+import org.junit.jupiter.api.condition.OS;
 
 class IndexControllerTest {
 
@@ -64,6 +69,42 @@ class IndexControllerTest {
 	@Test
 	void testAssumptionIsTrue() {
 		assumeTrue("TESTE".equalsIgnoreCase("TESTE"));
+	}
+	
+	@EnabledOnOs(OS.MAC)
+	@Test
+	void testMeOnMacOS() {
+		// 
+	}
+
+	@EnabledOnOs(OS.WINDOWS)
+	@Test
+	void testMeOnWindows() {
+		//
+	}
+	
+	@EnabledOnJre(JRE.JAVA_8)
+	@Test
+	void testMeOnJava8() {
+		
+	}
+
+	@EnabledOnJre(JRE.JAVA_11)
+	@Test
+	void testMeOnJava11() {
+		
+	}
+	
+	@EnabledIfEnvironmentVariable(named = "USER", matches = "Renan")
+	@Test
+	void testIfUserRenan() {
+		
+	}
+	
+	@EnabledIfEnvironmentVariable(named = "USER", matches = "JT")
+	@Test
+	void testIfUserJT() {
+		
 	}
 
 }
