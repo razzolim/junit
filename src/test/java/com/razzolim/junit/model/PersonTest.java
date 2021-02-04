c8fc5b2fd5349317c3c57a9ea9dcb6e68ebfcda4
Renan Azzolim <azzolimrenan2@gmail.com>
2021-02-04 19:14:14 -0300

    Repeating tests

--- src/test/java/com/razzolim/junit/model/PersonTest.java
+++ src/test/java/com/razzolim/junit/model/PersonTest.java
@@ -3,6 +3,8 @@ package com.razzolim.junit.model;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import com.razzolim.junit.ModelTest;
@@ -16,6 +18,13 @@ class PersonTest implements ModelTest {
 		assertAll("Test properties set",
 				() -> assertEquals("Renan", person.getFirstName(), "First name failed"),
 				() -> assertEquals("Azzolim", person.getLastName(), "Last name failed"));
+	}
+
+	@RepeatedTest(value = 10, name = "{displayName} : {currentRepetition} - {totalRepetitions}")
+	@DisplayName("My repeated test")
+	@Test
+	void myRepeatedTest() {
+		
 	}
 
 }
