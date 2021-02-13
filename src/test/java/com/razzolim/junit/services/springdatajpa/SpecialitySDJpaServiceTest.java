5db9801ce21ee236d37df6042b76d8ce6a3a0d9d
Renan Azzolim <azzolimrenan2@gmail.com>
2021-02-13 08:39:58 -0300

    Verify Interactions with Mockito Mocks

--- src/test/java/com/razzolim/junit/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/com/razzolim/junit/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -1,5 +1,11 @@
 package com.razzolim.junit.services.springdatajpa;
 
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.atMost;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -21,6 +27,35 @@ class SpecialitySDJpaServiceTest {
 	@Test
 	void deleteById() {
 		service.deleteById(1l);
+		service.deleteById(1l);
+		
+		verify(specialityRepository, times(2)).deleteById(1l);
+	}
+	
+	@Test
+	void deleteByIdAtLeast() {
+		service.deleteById(1l);
+		service.deleteById(1l);
+		
+		verify(specialityRepository, atLeastOnce()).deleteById(1l);
+	}
+	
+	@Test
+	void deleteByIdAtMost() {
+		service.deleteById(1l);
+		service.deleteById(1l);
+		
+		verify(specialityRepository, atMost(5)).deleteById(1l);
+	}
+	
+	@Test
+	void deleteByIdNever() {
+		service.deleteById(1l);
+		service.deleteById(1l);
+		
+		verify(specialityRepository, atLeastOnce()).deleteById(1l);
+		
+		verify(specialityRepository, never()).deleteById(5l);
 	}
 	
 	@Test
