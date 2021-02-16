890cf6f545609c47c56366e0bbb70391397eb558
Renan Azzolim <azzolimrenan2@gmail.com>
2021-02-16 20:54:23 -0300

    Spring Web Configuration with XML

---
+++ src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
@@ -0,0 +1,55 @@
+/*
+ * COPYRIGHT RENAN AZZOLIM 2021 - ALL RIGHTS RESERVED.
+ * 
+ * This software is only to be used for the purpose for which it has been
+ * provided. No part of it is to be reproduced, disassembled, transmitted,
+ * stored in a retrieval system nor translated in any human or computer
+ * language in any way or for any other purposes whatsoever without the prior
+ * written consent of Renan Azzolim.
+ */
+package org.springframework.samples.petclinic.web;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.service.ClinicService;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+/**
+ * @author Renan
+ *
+ * @since 16-02-2021
+ *
+ */
+@SpringJUnitWebConfig(locations = {"classpath:spring/mvc-test-config.xml", "classpath:spring/mvc-core-config.xml"})
+class OwnerControllerTest {
+	
+	@Autowired
+	OwnerController controller;
+	
+	@Autowired
+	ClinicService clinicService;
+	
+	MockMvc mockMvc;
+	
+	@BeforeEach
+	void setUp() {
+		mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+	}
+	
+	@Test
+	void initCreationFormTest() throws Exception {
+		mockMvc.perform(get("/owners/new"))
+			.andExpect(status().isOk())
+			.andExpect(model().attributeExists("owner"))
+			.andExpect(view().name("owners/createOrUpdateOwnerForm"));
+	}
+
+}
