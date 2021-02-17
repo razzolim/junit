72e3eaaae883b1bcff44597f0eb82c8282741770
Renan Azzolim <azzolimrenan2@gmail.com>
2021-02-16 21:37:27 -0300

    Completing tests for FindForm

--- src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
@@ -9,14 +9,28 @@
  */
 package org.springframework.samples.petclinic.web;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.times;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.model.Owner;
 import org.springframework.samples.petclinic.service.ClinicService;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
@@ -28,6 +42,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
  * @since 16-02-2021
  *
  */
+@ExtendWith(MockitoExtension.class)
 @SpringJUnitWebConfig(locations = {"classpath:spring/mvc-test-config.xml", "classpath:spring/mvc-core-config.xml"})
 class OwnerControllerTest {
 	
@@ -39,9 +54,55 @@ class OwnerControllerTest {
 	
 	MockMvc mockMvc;
 	
+	@Captor
+	ArgumentCaptor<String> stringArgumentCaptor;
+	
 	@BeforeEach
 	void setUp() {
 		mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+	}
+	
+	@AfterEach
+	void tearDown() {
+		reset(clinicService);
+	}
+	
+	@Test
+	void testReturnListOfOwners() throws Exception {
+		given(clinicService.findOwnerByLastName("")).willReturn(Lists.newArrayList(new Owner(), new Owner()));
+		
+		mockMvc.perform(get("/owners"))
+		.andExpect(status().isOk())
+		.andExpect(view().name("owners/ownersList"));
+		
+		then(clinicService).should().findOwnerByLastName(stringArgumentCaptor.capture());
+		
+		assertThat(stringArgumentCaptor.getValue()).isEqualToIgnoringCase("");
+	}
+	
+	@Test
+	void testFindOwnerOneResult() throws Exception {
+		final String findJustOne = "FindJustOne";
+		final Owner justOne = new Owner();
+		justOne.setId(1);
+		justOne.setLastName(findJustOne);
+		
+		given(clinicService.findOwnerByLastName(findJustOne)).willReturn(Lists.newArrayList(justOne));
+		
+		mockMvc.perform(get("/owners")
+				.param("lastName", findJustOne))
+		.andExpect(status().is3xxRedirection())
+		.andExpect(view().name("redirect:/owners/1"));
+		
+		then(clinicService).should().findOwnerByLastName(anyString());
+	}
+	
+	@Test
+	void testFindByNameWhenLastNameIsBlank() throws Exception {
+		mockMvc.perform(get("/owners")
+				.param("lastName", ""))
+			.andExpect(status().isOk())
+			.andExpect(view().name("owners/findOwners"));
 	}
 	
 	@Test
